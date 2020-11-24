1571aecf2c21304f111a03ffd26bac6de16c3f3b
varuns <varuns@spritle.com>
2020-11-24 20:27:53 +0400

    added header using semantic ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,16 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'jquery'
+window.$ = $
+
+import 'semantic-ui-sass'
+
+document.addEventListener('turbolinks:load', () => {
+  $('.ui.dropdown').dropdown()
+})
+
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
