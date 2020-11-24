8596da3405c32832f9c8e58dfa45659054b8b1bc
varuns <varuns@spritle.com>
2020-11-24 07:52:59 +0400

    set up root route and login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/index'
+  get 'chatroom/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'chatroom#index'
+  get 'login', to: "sessions#new"
 end
